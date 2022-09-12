26ea170491679ab6b5bb061934e7e3d20f330c87
Liedsonrm <liedsonrm.jpa@gmail.com>
2022-09-12 01:41:56 -0300

    Project ended

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+//Setting the app to use express.js for get the data sended by params or body
+app.use(express.json())
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -20,8 +23,16 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:id", async(req, res) => {
+  var { id } =  req.params
+  var data = new Date(id)
+  console.log(data)
+  if(data === undefined || data === null || data == "Invalid Date"){
+    return res.json({ error : "Invalid Date" })
+  }
+
+  res.json({unix: data.getTime(), utc: data.toUTCString()});
+
 });
 
 
